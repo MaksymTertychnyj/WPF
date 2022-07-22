bfa24d6fa104d94a1708c49f32467b94856a67d9
MaksymTertychnyj <maka12008@ukr.net>
2022-07-22 18:12:20 +0300

    created localization provider; created converter to translate;

---
+++ Figure/Helper/TranslatConverter.cs
@@ -0,0 +1,29 @@
+﻿using Figure.ViewModel;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Figure.Helper
+{
+    public class TranslatConverter : IMultiValueConverter
+    {
+        string? nameTargetObject;
+        ResourceDictionary? windowResources;
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            nameTargetObject = values[0]?.ToString();
+            windowResources = values[1] is MainViewModel viewModel ?
+                viewModel.LocalizationProvider.MainWindowResources
+                : null;
+
+            return (windowResources != null) && (nameTargetObject != null) ? windowResources[nameTargetObject] : "";
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
